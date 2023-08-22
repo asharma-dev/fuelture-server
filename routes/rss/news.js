8dac5c639a1e00578a6df41c1ff094ad8807c9bc
abhisheksharma1123 <abhisheksharma1123@gmail.com>
2023-08-21 22:22:51 -0400

    fix for rss feed

--- routes/rss/news.js
+++ routes/rss/news.js
@@ -1,87 +1,104 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    // Fetch articles
-    const fetchArticles = async () => {
-        const Parser = require('rss-parser')
-        const parser = new Parser()
+router.get("/", (req, res) => {
+  // Fetch articles
+  const fetchArticles = async () => {
+    const Parser = require("rss-parser");
+    const parser = new Parser();
 
-        let items = []
-        let autoEvolutionImages = []; // For autoevolution image urls
+    let items = [];
+    let autoEvolutionImages = []; // For autoevolution image urls
 
+    // Fetch
+    const autoblogRaw = await parser.parseURL(
+      "https://www.autoblog.com/category/green-auto-news/rss.xml"
+    );
+    const autoevolutionRaw = await parser.parseURL(
+      "https://www.autoevolution.com/rss/backend-green.xml"
+    );
+    const insideevsRaw = await parser.parseURL(
+      "https://insideevs.com/rss/articles/all/"
+    );
 
-        // Fetch
-        const autoblogRaw = await parser.parseURL('https://www.autoblog.com/category/green-auto-news/rss.xml')
-        const autoevolutionRaw = await parser.parseURL('https://www.autoevolution.com/rss/backend-green.xml')
-        const insideevsRaw = await parser.parseURL('https://insideevs.com/rss/articles/all/')
-
-        // For autoevolution image urls
-        const fetch = require('node-fetch');
-        let response = await fetch('https://www.autoevolution.com/rss/backend-green.xml');
+    // For autoevolution image urls
+    let fetch;
+    import("node-fetch")
+      .then(async (module) => {
+        fetch = module.default;
+        // Continue with your code that uses fetch
+        let response = await fetch(
+          "https://www.autoevolution.com/rss/backend-green.xml"
+        );
         let textString = await response.text();
-        let parseString = require('xml2js').parseString;
+        let parseString = require("xml2js").parseString;
         parseString(textString, function (err, result) {
-            let articles = (result.rss.channel[0].item);
-            articles.forEach((article, index) => {
-                autoEvolutionImages.push(article["media:content"][0]["$"]["url"]);
-            });
+          let articles = result.rss.channel[0].item;
+          articles.forEach((article, index) => {
+            autoEvolutionImages.push(article["media:content"][0]["$"]["url"]);
+          });
         });
+      })
+      .catch((error) => {
+        // Handle any errors that occur during dynamic import
+      });
 
-        // Autoblog
-        autoblogRaw.items.forEach(item => {
-            let imgURL = item.content.split('<img src="').pop().split('"')[0];
+    // Autoblog
+    autoblogRaw.items.forEach((item) => {
+      let imgURL = item.content.split('<img src="').pop().split('"')[0];
 
-            let obj = {
-                title: item.title,
-                img: imgURL,
-                content: item.contentSnippet,
-                pubDate: `${item.pubDate.split("2021")[0]}2021`,
-                link: item.link,
-                publisher: `Autoblog`,
-                author: item["dc:creator"]
-            }
+      let obj = {
+        title: item.title,
+        img: imgURL,
+        content: item.contentSnippet,
+        pubDate: `${item.pubDate.split("2021")[0]}2021`,
+        link: item.link,
+        publisher: `Autoblog`,
+        author: item["dc:creator"],
+      };
 
-            items.push(obj);
-        })
+      items.push(obj);
+    });
 
-        // Autoevolution
-        autoevolutionRaw.items.forEach((item, index) => {
+    // Autoevolution
+    autoevolutionRaw.items.forEach((item, index) => {
+      let obj = {
+        title: item.title,
+        img: autoEvolutionImages[index],
+        content: item.contentSnippet,
+        pubDate: `${item.pubDate.split("2021")[0]}2021`,
+        link: item.link,
+        publisher: `Autoevolution`,
+        author: item["dc:creator"].toString().split("(").pop().split(")")[0],
+      };
 
-            let obj = {
-                title: item.title,
-                img: autoEvolutionImages[index],
-                content: item.contentSnippet,
-                pubDate: `${item.pubDate.split("2021")[0]}2021`,
-                link: item.link,
-                publisher: `Autoevolution`,
-                author: item["dc:creator"].toString().split('(').pop().split(')')[0]
-            }
+      items.push(obj);
+    });
 
-            items.push(obj);
-        })
+    // InsideEVs
+    insideevsRaw.items.forEach((item) => {
+      let imgURL = item["enclosure"]["url"]
+        .split('<img src="')
+        .pop()
+        .split('"')[0];
 
-        // InsideEVs
-        insideevsRaw.items.forEach(item => {
-            let imgURL = item["enclosure"]["url"].split('<img src="').pop().split('"')[0];
+      let obj = {
+        title: item.title,
+        img: imgURL,
+        content: item.contentSnippet,
+        pubDate: `${item.pubDate.split("2021")[0]}2021`,
+        link: item.link,
+        publisher: `InsideEVs`,
+        author: ``,
+      };
 
-            let obj = {
-                title: item.title,
-                img: imgURL,
-                content: item.contentSnippet,
-                pubDate: `${item.pubDate.split("2021")[0]}2021`,
-                link: item.link,
-                publisher: `InsideEVs`,
-                author: ``
-            }
+      items.push(obj);
+    });
 
-            items.push(obj);
-        })
+    res.status(200).send(items);
+  };
 
-        res.status(200).send(items)
-    }
+  fetchArticles();
+});
 
-    fetchArticles();
-})
-
-module.exports = router
+module.exports = router;
